3f3ee2a560fe8ab4c97f1507217dbf479c0b9dbc
gsilvaepinto <guiiilhermefspinto@gmail.com>
2026-03-23 16:50:42 +0000

    update

--- PythonModule00/ex2/ft_harvest_total.py
+++ PythonModule00/ex2/ft_harvest_total.py
@@ -1,5 +1,9 @@
 def ft_harvest_total() -> None:
     total = 0
-    for day in range(1, 4):
+    day = 1
+    while day <= 3:
         total += int(input(f"Day {day} harvest: "))
+        day += 1
     print(f"Total harvest: {total}")
+
+ft_harvest_total()
