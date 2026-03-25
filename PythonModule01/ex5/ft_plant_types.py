bd307d69cf6e22a7c53f2e3d9490f3819594c442
gsilvaepinto <guiiilhermefspinto@gmail.com>
2026-03-25 17:32:44 +0000

    update

--- PythonModule01/ex5/ft_plant_types.py
+++ PythonModule01/ex5/ft_plant_types.py
@@ -3,22 +3,28 @@ class Plant:
         self.name = name
         self.height = height
         self.age = age
-
-    def __str__(self):
-        classname = self.__class__.__name__
-        return f"{self.name} ({classname}): {self.height}cm, {self.age} days"
-
+    
+    def show(self):
+        print (f"{self.name}: {self.height:.1f}cm, {self.age} days old")
+    
 
 class Flower(Plant):
     def __init__(self, name, height, age, color):
         super().__init__(name, height, age)
         self.color = color
-
+        self.blooming = False
+    
     def bloom(self):
-        print(f"{self.name} is blooming beautifully!\n")
+        print(f"[asking the {self.name.lower()} to bloom]")
+        self.blooming = True
 
-    def __str__(self):
-        return super().__str__() + f", {self.color} color"
+    def show(self):
+        super().show()
+        print(f"Color: {self.color}")
+        if not self.blooming:
+            print(f"{self.name} has not bloomed yet")
+        else:
+            print(f"{self.name} is blooming beautifully!")
 
 
 class Tree(Plant):
@@ -26,36 +32,25 @@ class Tree(Plant):
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
-    def produce_shade(self):
-        print(f"{self.name} provides 78 square meters of shade\n")
-
-    def __str__(self):
-        return super().__str__() + f", {self.trunk_diameter}cm diameter"
-
 
 class Vegetable(Plant):
     def __init__(self, name, height, age, harvest_season):
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
 
-    def nutritional_value(self):
-        print(f"{self.name} is rich in vitamin C")
 
-    def __str__(self):
-        return super().__str__() + f", {self.harvest_season} harvest"
+def main():
+    print("=== Garden Plant Types ===")
+    plant_list: list[Flower, Tree, Vegetable] = [
+        Flower('Rose', 15, 10, 'red')
+    ]
+    for plant in plant_list:
+        if isinstance(plant, Flower):
+            print(f"=== {plant.__class__.__name__}")
+            plant.show()
+            plant.bloom()
+            plant.show()
 
 
 if __name__ == "__main__":
-    print("=== Garden Plant Types ===\n")
-    '#ROSE'
-    rose = Flower("Rose", 25, 30, "red")
-    print(rose)
-    rose.bloom()
-    '#OAK'
-    oak = Tree("Oak", 500, 1825, 50)
-    print(oak)
-    oak.produce_shade()
-    '#TOMATO'
-    tomato = Vegetable("Tomato", 80, 90, "summer")
-    print(tomato)
-    tomato.nutritional_value()
+    main()
