829f752211c95de130f01878544ecd3661e3cd0d
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2026-03-25 14:55:16 +0000

    update

--- PythonModule01/ex3/ft_plant_factory.py
+++ PythonModule01/ex3/ft_plant_factory.py
@@ -1,26 +1,34 @@
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: float, age: int):
         self.name = name
         self.height = height
         self.plant_age = age
 
+    def grow(self, height: float):
+        self.height += height
 
-if __name__ == "__main__":
-    plants_list = [
-        ("Rose", 25, 30),
-        ("Oak", 200, 365),
-        ("Cactus", 5, 90),
-        ("Sunflower", 80, 45),
-        ("Fern", 15, 120)
+    def show(self):
+        return(f"{self.name}: {self.height:.1f}cm, {self.plant_age} days old")
+
+
+def main():
+    print("=== Plant Factory Output ===")
+    plants_list: list[tuple[str, float, int]] = [
+        ('Rose', 25, 30),
+        ('Oak', 200, 365),
+        ('Cactus', 5, 90),
+        ('Sunflower', 80, 45),
+        ('Fern', 15, 120)
     ]
 
-    plants = []
+    plants: list[Plant] = []
 
-    print("=== Plant Factory Output ===")
     for data in plants_list:
         plant: Plant = Plant(*data)
         plants.append(plant)
-        print(
-            f"Created: {plant.name} ({plant.height}cm, {plant.plant_age} days)"
-        )
-    print(f"\nTotal plants created: {len(plants)}")
+        print(f"Created: {plant.show()}")
+
+
+if __name__ == "__main__":
+    main()
+    
