6764b716abee7dba933d85706a67660d49f9cc08
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2026-03-25 14:54:47 +0000

    update

--- PythonModule01/ex2/ft_plant_growth.py
+++ PythonModule01/ex2/ft_plant_growth.py
@@ -1,30 +1,30 @@
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: float, age: int):
         self.name = name
         self.height = height
         self.plant_age = age
+        self.initial_height = height
 
-    def grow(self, amount=1) -> None:
-        self.height += amount
+    def grow(self, height: float):
+        self.height += height
 
-    def age(self) -> None:
+    def age(self):
         self.plant_age += 1
 
-    def get_info(self):
-        return f"{self.name}: {self.height}cm, {self.plant_age} days old"
+    def __str__(self):
+        return f"{self.name}: {self.height:.1f}cm, {self.plant_age} days old"
+
+
+def main():
+    print("=== Garden Plant Growth ===")
+    plant: Plant = Plant('Rose', 25, 30)
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        print(plant)
+        plant.grow(0.8)
+        plant.age()
+    print(f"Growth this week: {round(plant.height - plant.initial_height)}cm")
 
 
 if __name__ == "__main__":
-    plant: Plant = Plant("Rose", 25, 30)
-    initial_height = plant.height
-
-    print("=== Day 1 ===")
-    print(plant.get_info())
-
-    for day in range(1, 7):
-        plant.grow()
-        plant.age()
-
-    print("=== Day 7 ===")
-    print(plant.get_info())
-    print(f"Growth this week: +{plant.height - initial_height}cm")
+    main()
