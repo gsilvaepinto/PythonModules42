65da540e47d38d9a9b1fda24d35c5f8d6089bf3c
gsilvaepinto <guiiilhermefspinto@gmail.com>
2026-04-10 21:58:58 +0100

    update

--- PythonModule03/ex4/ft_inventory_system.py
+++ PythonModule03/ex4/ft_inventory_system.py
@@ -14,23 +14,29 @@ if __name__ == '__main__':
                 inventory[data[0]] = int(data[1])
             except ValueError as e:
                 print(f"Quantity error for '{data[0]}': {e}")
-    print("Got inventory:", inventory)
-    print("Item list:", list(inventory.keys()))
-    print(f"Total quantity of the {len(inventory)} items: {sum(inventory.values())}")
-    print(f"Item sword represents: {inventory['sword'] / sum(inventory.values()) * 100:.1f}%")
-    print(f"Item sword represents: {inventory['potion'] / sum(inventory.values()) * 100:.1f}%")
-    print(f"Item shield represents: {inventory['shield'] / sum(inventory.values()) * 100:.1f}%")
-    print(f"Item armor represents: {inventory['armor'] / sum(inventory.values()) * 100:.1f}%")
-    print(f"Item helmet represents: {inventory['helmet'] / sum(inventory.values()) * 100:.1f}%")
+    print(f"Got inventory: {inventory}")
+    print(f"Item list: {list(inventory.keys())}")
+    print(f"Total quantity of the {len(inventory)} items: "
+          f"{sum(inventory.values())}")
+    print(f"Item sword represents "
+          f"{inventory['sword'] / sum(inventory.values()) * 100:.1f}%")
+    print(f"Item potion represents "
+          f"{inventory['potion'] / sum(inventory.values()) * 100:.1f}%")
+    print(f"Item shield represents "
+          f"{inventory['shield'] / sum(inventory.values()) * 100:.1f}%")
+    print(f"Item armor represents "
+          f"{inventory['armor'] / sum(inventory.values()) * 100:.1f}%")
+    print(f"Item helmet represents "
+          f"{inventory['helmet'] / sum(inventory.values()) * 100:.1f}%")
     most = None
     for item in inventory:
         if most is None or inventory[most] < inventory[item]:
             most = item
-    print(f"Item most abundant: {most} with quantity: {inventory[most]}")
+    print(f"Item most abundant: {most} with quantity {inventory[most]}")
     least = None
     for item in inventory:
         if least is None or inventory[least] > inventory[item]:
             least = item
-    print(f"Item least abundant: {least} with quantity: {inventory[least]}")
+    print(f"Item least abundant: {least} with quantity {inventory[least]}")
     inventory.update({'magic_item': 1})
-    print("Updated inventory:", inventory)
+    print(inventory)
